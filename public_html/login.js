f4f39e862b7361f667d0d44e2bd91ddbf19ab07d
dongkeon park <zsx160@gmail.com>
2017-06-01 22:25:35 +0900

    add DB

--- public_html/login.js
+++ public_html/login.js
@@ -1,19 +1,80 @@
 // Get the <span> element that closes the modal
 span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
 function loginPopup() {
-    document.getElementById("myModal").style.display = "block";
+    document.getElementById('myModal1').style.display = "block";
+    document.getElementById('LoginButton').addEventListener('click', login);
 }
 
-// When the user clicks on <span> (x), close the modal
+function login(){
+	var user = document.getElementById('logemail').value;
+	var keyvalue = localStorage.getItem(user);
+	var lastrecord = localStorage.getItem(keyvalue);
+		
+	var databox = document.getElementById('databox');
+	databox.innerHTML = lastrecord;
+
+	document.getElementById('myModal1').style.display = "none";
+}
+
 function closeLogin() {
-    document.getElementById("myModal").style.display = "none";
+    document.getElementById('myModal1').style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target === document.getElementById("myModal")) {
-        document.getElementById("myModal").style.display = "none";
+window.onclick = function (event) {
+    if (event.target === document.getElementById('myModal1')) {
+        document.getElementById('myModal1').style.display = "none";
     }
-};
+}
+
+function signupPopup(){
+	document.getElementById('myModal2').style.display = "block";
+	document.getElementById('SignUpButton').addEventListener('click', newitem);
+}
+
+function newitem(){
+	var key = document.getElementById('signemail').value;
+	var value = key + 'value';
+	localStorage.setItem(key, value);
+	localStorage.setItem(value, '');
+	document.getElementById('myModal2').style.display = "none";
+}
+
+function closeSignup(){
+	document.getElementById('myModal2').style.display = "none";
+}
+
+window.onclick = function(event){
+	if(event.target === document.getElementById('myModal2')) {
+		document.getElementById('myModal2').style.display = "none";
+	}
+}
+
+
+function search(){
+	var username = document.getElementById('logemail').value;
+	var keyvalue = localStorage.getItem(username);
+	var lastrecord = localStorage.getItem(keyvalue);
+
+	var record = document.getElementById('sidebar-search').value;
+	var newrecord = lastrecord + '<br>' + record ;
+
+	localStorage.setItem(keyvalue, newrecord);
+
+	var recentrecord = localStorage.getItem(keyvalue);
+	var databox = document.getElementById('databox');
+	databox.innerHTML = recentrecord;	
+}
+
+function clearmemory(){
+	var username = document.getElementById('logemail').value;
+	var keyvalue = localStorage.getItem(username);
+	localStorage.removeItem(keyvalue);
+
+	var result = localStorage.getItem(keyvalue);
+	var databox = document.getElementById('databox');
+	databox.innerHTML = result;
+
+}
+
